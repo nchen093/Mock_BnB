d996a9d2b4d7a542616f02831924144eecf9348b
Glowupmatt <glowupmatt@gmail.com>
2024-10-29 16:04:27 -0700

    add middle ware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
+router.get("/", function (req, res) {
+  res.json({ message: "Test response" });
+});
+
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
 });
