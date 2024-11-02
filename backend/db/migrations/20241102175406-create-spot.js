624abf577aa648c2ae75256ac3e2cde3d818fd45
Sqhy4445 <nchen093@gmail.com>
2024-11-02 14:28:02 -0400

    spot and user

---
+++ backend/db/migrations/20241102175406-create-spot.js
@@ -0,0 +1,81 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ownerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      address: {
+        type: Sequelize.STRING(256),
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING(256),
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING(256),
+        allowNull: false
+      },
+      country: {
+        type: Sequelize.STRING(256),
+        allowNull: false
+      },
+      lat: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      },
+      lng: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING(256),
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      },
+      avgRating: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      },
+      previewImage: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Spots');
+  }
+};
