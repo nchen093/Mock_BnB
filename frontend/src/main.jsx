eca9212ccbe9de990fff72114509d9499300378b
Sqhy4445 <nchen093@gmail.com>
2024-11-23 04:19:58 -0500

    finish phase-1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import configureStore from "./store";
+import * as sessionActions from "./store/session";
 import "./index.css";
 import { Provider } from "react-redux";
-import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 
 const store = configureStore();
@@ -13,6 +14,7 @@ if (import.meta.env.MODE !== "production") {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
