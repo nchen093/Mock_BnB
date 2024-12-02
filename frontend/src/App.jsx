f053ad974424e66b940bd6aa014fd2e10f6ee877
Sqhy4445 <nchen093@gmail.com>
2024-12-02 18:55:40 -0500

    back to /

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: "/HomePage",
+        path: "/",
         element: <ListOfSpots />,
       },
 
