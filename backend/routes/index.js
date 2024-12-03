53d9a2b8ffe3f27ca97091ec5905f443b7dc8a74
Sqhy4445 <nchen093@gmail.com>
2024-12-02 19:05:59 -0500

    changing index files

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve(__dirname, "../frontend, dist")));
+  router.use(express.static(path.resolve("../frontend/dist")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
