e29e885da97d33541e3b636f154091871b43e580
Sqhy4445 <nchen093@gmail.com>
2024-12-10 01:47:30 -0500

    userSpotInfo

--- frontend/src/components/LoadSpotPage/UserSpotInfo.jsx
+++ frontend/src/components/LoadSpotPage/UserSpotInfo.jsx
@@ -37,7 +37,7 @@ export default function UserSpotInfo({ spot }) {
           <div className="spotGridItemStarRating">
             <GoStarFill style={{ color: "#ffd60a" }} />
             {spot.avgRating && spot.avgRating !== "No Comment"
-              ? spot.avgRating.toFixed(1)
+              ? spot.avgRating
               : "New"}
           </div>
         </div>
